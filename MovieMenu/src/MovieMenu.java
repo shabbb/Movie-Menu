165c9b11ca02aa8c33d313576f8e83360fb396a7
Shabab-Siddique <42660039+Shabab-Siddique@users.noreply.github.com>
2020-06-07 06:50:18 -0700

    Movie Menu goes through folders and subfolders to find video files.

--- MovieMenu/src/MovieMenu.java
+++ MovieMenu/src/MovieMenu.java
@@ -4,14 +4,17 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
-
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JSplitPane;
+import javax.swing.ScrollPaneConstants;
 import javax.swing.SwingConstants;
 import org.apache.commons.io.FilenameUtils;
 
@@ -23,20 +26,27 @@ public class MovieMenu extends Frame implements ActionListener{
 	JPanel addMovie;
 	String realMovieName;
 	Desktop computer;
+	String extensionName;
+	String movieDirectoryName = "E:\\Shabab\\Videos";
+	List<File> movieFileList = new ArrayList<File>();
+	File[] movieList;
 	
 	public MovieMenu(){
 		
 		JFrame f = new JFrame("Movie Menu");
 		movie = new JPanel();
-		addMovie = new JPanel();
+		//addMovie = new JPanel();
 		
 		BoxLayout movies = new BoxLayout(movie, BoxLayout.Y_AXIS);
 		movie.setLayout(movies);
+		JScrollPane movieListScroll = new JScrollPane(movie);
+		movieListScroll.getVerticalScrollBar().setUnitIncrement(20);
+		//movieListScroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
 		
-		BoxLayout button = new BoxLayout(addMovie, BoxLayout.Y_AXIS);
+	/*	BoxLayout button = new BoxLayout(addMovie, BoxLayout.Y_AXIS);
 		addMovie.setLayout(button);
 		
-		findMovie = new JButton("Add Movie");
+		findMovie = new JButton("Add Movies");
 		movieChooser = new JFileChooser();
 		movieChooser.setCurrentDirectory(new File(System.getProperty("user.home")));
 		findMovie.addActionListener(this);
@@ -50,7 +60,56 @@ public class MovieMenu extends Frame implements ActionListener{
 		moviePane.setResizeWeight(1.0);
 		
 		f.add(moviePane);
+		*/
+		//File movieFiles = new File(movieDirectoryName);
+		//File[] movieList = movieFiles.listFiles();
 		
+		findMovies(movieDirectoryName);
+		
+		
+		for (File m: movieFileList)
+		{
+			File movieFile = m;
+			extensionName = FilenameUtils.getExtension(movieFile.getName());
+			//System.out.println(extensionName);
+			
+			if (extensionName.equals("mp4")||extensionName.equals("mov")||extensionName.equals("m4v")) {
+			realMovieName = FilenameUtils.removeExtension(movieFile.getName());
+			JLabel movieName = new JLabel(realMovieName);
+			movieName.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+			
+			movieName.addMouseListener(new MouseAdapter() {
+				
+				@Override
+				public void mouseClicked(MouseEvent e) {
+					computer = Desktop.getDesktop();
+					try {
+						computer.open(movieFile);
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+				}
+				
+				@Override
+				public void mouseEntered(MouseEvent e) {
+					
+				}
+				
+				@Override
+				public void mouseExited (MouseEvent e) {
+					
+				}
+			});	
+			
+			movie.add(movieName);
+			movie.validate();
+			movie.repaint();
+			//System.out.println("Selected movie: " + realMovieName);
+			
+		}
+		}
+		f.getContentPane().add(movieListScroll);
 		f.setSize(1000, 500);
 		f.setVisible(true);
 	}
@@ -61,14 +120,93 @@ public class MovieMenu extends Frame implements ActionListener{
 	{
 		MovieMenu application = new MovieMenu();
 	}
-	
-	
+	//int counter = 0;
+	public void findMovies(String movieDirectory)
+	{
+		File movieFiles = new File(movieDirectory);
+		//System.out.println(movieFiles.getName());		
+		movieList = movieFiles.listFiles();
+		//counter+=1;
+		//System.out.println(counter);
+		/* System.out.println(movieList[0].getName());
+		 for (File m: movieList)
+		{
+			System.out.println(m.getName());
+		} */
+		
+		if (movieList != null)
+		{
+			for (File m: movieList)
+			{
+			//	System.out.println(m.getName());
+			//	System.out.println(counter);
+				if (m != null) {
+			//	System.out.println(m.getName());
+			//	System.out.println(counter);
+				extensionName = FilenameUtils.getExtension(m.getName());
+				if (m.isFile() & (extensionName.equals("mp4")||extensionName.equals("mov")||extensionName.equals("m4v")))
+				{
+					//System.out.println(m.getName());
+					movieFileList.add(m);
+				}
+				
+				else
+				{
+					//System.out.println(m.getName());
+					//System.out.println(m.getAbsolutePath());
+					findMovies(m.getAbsolutePath());
+				}
+				}
+			}
+		}
+		
+	}
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		if (e.getSource() == findMovie)
+		/* if (e.getSource() == findMovie)
 		{
+			
+			File movieFiles = new File("C:\\Users\\Shabab\\Videos\\Captures");
+			File[] movieList = movieFiles.listFiles();
+			for (File m: movieList)
+			{
+				File movieFile = m;
+				realMovieName = FilenameUtils.removeExtension(movieFile.getName());
+				JLabel movieName = new JLabel(realMovieName);
+				movieName.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+				
+				movieName.addMouseListener(new MouseAdapter() {
+					
+					@Override
+					public void mouseClicked(MouseEvent e) {
+						computer = Desktop.getDesktop();
+						try {
+							computer.open(movieFile);
+						} catch (IOException e1) {
+							// TODO Auto-generated catch block
+							e1.printStackTrace();
+						}
+					}
+					
+					@Override
+					public void mouseEntered(MouseEvent e) {
+						
+					}
+					
+					@Override
+					public void mouseExited (MouseEvent e) {
+						
+					}
+				});	
+				
+				movie.add(movieName);
+				movie.validate();
+				movie.repaint();
+				System.out.println("Selected movie: " + realMovieName);
+				
+			} 
 			int value = movieChooser.showOpenDialog(addMovie);
 			
 			if (value == JFileChooser.APPROVE_OPTION)
@@ -100,14 +238,10 @@ public class MovieMenu extends Frame implements ActionListener{
 					public void mouseExited (MouseEvent e) {
 						
 					}
-				});
+				}); */
 				
-				movie.add(movieName);
-				movie.validate();
-				movie.repaint();
-				System.out.println("Selected movie: " + realMovieName);
-			}
-		}
+			
+		
 	}
 
 }
